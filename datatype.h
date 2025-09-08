a6ab3552518ead4a34ddd38a56c47060ce2262be
[marcogiacon04] <[marco.giacon.4@studenti.unipd.it]>
2025-09-08 15:38:13 +0200

    aggiunto file datatype.h

---
+++ datatype.h
@@ -0,0 +1,58 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+namespace ENUM{
+
+    enum class bookcategories{
+        Romanzo,
+        Giallo,
+        Thriller,
+        Horror,
+        Fantascienza,
+        Fantasy,
+        Distopico,
+        Storico,
+        Avventura,
+        Romanzo_rosa,
+        Drammatico,
+        Umoristico,
+        Biografia
+    };
+
+    enum class filmcategories{
+        Azione,
+        Avventura,
+        Commedia,
+        Drammatico,
+        Erotico,
+        Horror,
+        Mistero,
+        Romantico,
+        Thriller,
+        Guerra,
+        Fantasy,
+        fantascienza,
+        Musical,
+        Biografico,
+        Storico,
+        Sport
+    };
+    enum class musiccategories{
+        Classica,
+        Rap,
+        Trap,
+        Indie,
+        Pop,
+        Rock,
+        Metal,
+        jazz,
+        Blues,
+        Hip_Hop,
+        Elettronica,
+        Latina,
+        Country,
+        Reaggeton
+    };
+}
+
+#endif // DATATYPE_H
